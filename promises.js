8d84fc9783beb073be987389632343422c679548
Karn Tiwari <karntiwari5353@gmail.com>
2022-06-26 14:14:48 +0530

    uihuidf

---
+++ promises.js
@@ -0,0 +1,15 @@
+console.log("Before");
+Fs.readFile("f1.txt",function(err,data){
+    console.log(data);
+
+});
+let promise = fs.promise.readFile("f1.txt");
+// console.log("promises");
+// fullfilled
+promise.then(function (data){
+    console.log("" + data);
+})
+// rejected
+promise.catch(function (err){
+    console.log(err);
+})
